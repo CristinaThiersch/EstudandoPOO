352695a2e198ca72dc764f3f49da00171c77ab2d
ANA CRISTINA THIERSCH DA CRUZ <114027464+CristinaThiersch@users.noreply.github.com>
2023-09-13 11:13:47 -0300

    Métodos especiais
    Exercício de getters, setters e constructor.

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author anath
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Nic", 0.4, "Amarela");        
+        System.out.println(c1.toString());
+        Caneta c2 = new Caneta("Bic", 0.5, "Azul");        
+        System.out.println(c2.toString());
+    }
+    
+}
